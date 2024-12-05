ff93e50903ced101ef938efecb0e56c37a542f82
creator <sc22y3c@leeds.ac.uk>
2024-12-05 01:29:16 +0000

    Finished

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config(object):
+    WTF_CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'trouble_25'
 
     basedir = os.path.abspath(os.path.dirname(__file__))
